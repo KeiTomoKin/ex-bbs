2b4c6a6f5409feed5920a5f2e6baf2e445d2664d
tomookakeita <keita516tomo@outlook.jp>
2019-06-03 11:05:44 +0900

    記事を表すドメインクラスの実装

---
+++ src/main/java/com/example/domain/Article.java
@@ -0,0 +1,58 @@
+package com.example.domain;
+
+import java.util.List;
+
+/**
+ * 記事を表すドメイン
+ * 
+ * @author keita.tomooka
+ *
+ */
+public class Article {
+	/** ID */
+	private Integer id;
+	/** 投稿者名 */
+	private String name;
+	/** 投稿内容 */
+	private String content;
+	/** コンテント */
+	private List<Comment> cmmentList;
+
+	@Override
+	public String toString() {
+		return "Article [id=" + id + ", name=" + name + ", content=" + content + ", cmmentList=" + cmmentList + "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public List<Comment> getCmmentList() {
+		return cmmentList;
+	}
+
+	public void setCmmentList(List<Comment> cmmentList) {
+		this.cmmentList = cmmentList;
+	}
+
+}
