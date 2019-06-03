7fe8a3a772c13bde91ba1808aab1c7b6a6f9f182
tomookakeita <keita516tomo@outlook.jp>
2019-06-03 13:24:06 +0900

    記事関連機能の処理を行うコントローラ

--- src/main/java/com/example/Controller/ArticleController.java
+++ src/main/java/com/example/Controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.example.Controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,33 +14,45 @@ import com.example.Form.ArticleForm;
 import com.example.domain.Article;
 import com.example.repository.ArticleRepository;
 
+
 /**
  * 記事関連機能の処理を行うコントローラ.
+ * 
  * @author keita.tomooka
  *
  */
 @Controller
 @RequestMapping("/bbs")
 public class ArticleController {
-	
+
 	@Autowired
 	private ArticleRepository articleRepository;
-	
+
 	@ModelAttribute
 	public ArticleForm setUpForm() {
 		return new ArticleForm();
 	}
-	
+
 	/**
 	 * 記事一覧画面に遷移する.
+	 * 
 	 * @return 記事一覧画面
 	 */
 	@RequestMapping("")
-	public String index(ArticleForm form,Model model) {
+	public String index(Model model) {
 		List<Article> articleList = new ArrayList<Article>();
-		articleList= articleRepository.findAll();
+		articleList = articleRepository.findAll();
 		model.addAttribute("articleList", articleList);
 		return "bbs";
+	}
+
+	@RequestMapping("/insert")
+	public String insert(ArticleForm form) {
+		Article article = new Article();
+		BeanUtils.copyProperties(form, article);
+		articleRepository.insert(article);
+		return "redirect:/bbs";
+
 	}
 
 }
